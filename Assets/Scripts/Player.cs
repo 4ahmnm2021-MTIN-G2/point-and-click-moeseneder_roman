daf022cd024df61bb0c8e829255320c7a5c5fd99
Your name hereromanmoeseneder <roman.moeseneder03@htl-salzburg.ac.at>
2021-03-22 08:56:51 +0100

    modify assets and settings. Input move script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public Vector3 mousePos;
+    public Vector3 mouseCameraPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePos = Input.mousePosition;
+            mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.transform.position = mouseCameraPos;
+        }
+    }
+}
